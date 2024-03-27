84d8e3b4e15444a90470d08c74e031e68b69e6b8
denisbelenko <denisdel123@>
2024-03-27 20:09:21 +0700

    поменял модуль attempt (присваевает mailing), написал функции фильтрации и отправки электроной почты

--- mailingApp/models.py
+++ mailingApp/models.py
@@ -44,29 +44,16 @@ class Massage(models.Model):
         verbose_name_plural = 'Сообщения'
 
 
-class Attempt(models.Model):
-    at_last_attempt = models.DateTimeField(verbose_name='Дата последней попытки', **NULLABLE)
-    status_attempt = models.BooleanField(verbose_name='Статус', default=False)
-    answer_mail = models.CharField(max_length=100, **NULLABLE)
-
-    def __str__(self):
-        return 'попытка отправки'
-
-    class Meta:
-        verbose_name = 'Попытка'
-        verbose_name_plural = 'Попытки'
-
-
 class Mailing(models.Model):
     name = models.CharField(max_length=50, verbose_name='Название')
     description = models.TextField(verbose_name='Описание рассылки')
     periodicity = models.CharField(max_length=10, choices=PERIODICITY_CHOICES, verbose_name='Периодичность')
-    at_start = models.DateField(verbose_name='Дата начала отправки', **NULLABLE)
-    at_end = models.DateField(verbose_name='Дата окончания отправки', **NULLABLE)
-    status = models.CharField(max_length=20, choices=STATUS_MAILING, verbose_name='Статус', **NULLABLE)
+    at_start = models.DateField(verbose_name='Дата начала отправки')
+    at_end = models.DateField(verbose_name='Дата окончания отправки')
+    status = models.CharField(max_length=20, choices=STATUS_MAILING, verbose_name='Статус', default='create')
     massage = models.ForeignKey(Massage, verbose_name='Сообщение', on_delete=models.CASCADE)
     client = models.ManyToManyField(Client, verbose_name='Клиент')
-    attempt = models.ForeignKey(Attempt, on_delete=models.CASCADE, verbose_name='попытка', **NULLABLE)
+    next_run = models.DateTimeField(verbose_name='Дата и время отправки')
 
     def __str__(self):
         return f'{self.name}'
@@ -74,3 +61,17 @@ class Mailing(models.Model):
     class Meta:
         verbose_name = 'Рассылка'
         verbose_name_plural = 'Рассылки'
+
+
+class Attempt(models.Model):
+    at_last_attempt = models.DateTimeField(verbose_name='Дата последней попытки', auto_now_add=True)
+    status_attempt = models.BooleanField(verbose_name='Статус', default=False)
+    answer_mail = models.CharField(max_length=100, **NULLABLE)
+    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE, verbose_name='Рассылка', **NULLABLE)
+
+    def __str__(self):
+        return 'попытка отправки'
+
+    class Meta:
+        verbose_name = 'Попытка'
+        verbose_name_plural = 'Попытки'
--- mailingApp/utils.py
+++ mailingApp/utils.py
@@ -1,26 +1,66 @@
-import smtplib
+import datetime
+from dateutil.relativedelta import relativedelta
 import os
+from django.utils import timezone
+from django.core.mail import send_mail
+from mailingApp.models import Mailing, Attempt
 
 PASSWORD_MAIL_RU = os.getenv('PASSWORD_MAIL_RU')
 ADDRESS_MAIL_RU = os.environ.get("ADDRESS_MAIL_RU")
 GETTER_MAIL = 'denis_belenko@mail.ru'
 
 
-def send_massage(objects):
-    with smtplib.SMTP('smtp.mail.ru', 587) as smtp:
-        smtp.ehlo()
-        smtp.starttls()
-        smtp.ehlo()
+def mailing_filter():
+    date_time = timezone.now()
 
-        smtp.login(ADDRESS_MAIL_RU, PASSWORD_MAIL_RU)
+    mailings = Mailing.objects.all()
+    for mailing in mailings:
+        if mailing.at_end >= date_time:
+            mailing.status = 'completed'
+            mailing.save()
+        else:
+            pass
+    mailings_filtered = mailings.filter(status='create')
 
-        subject = 'hello Denis'
-        body = 'it is test for your curse work'
-
-        msg = f'subject: {objects.title}\n\n{objects.text}'
-
-        smtp.sendmail(ADDRESS_MAIL_RU, GETTER_MAIL, msg)
+    send_email(mailings_filtered)
 
 
-if __name__ == '__main__':
-    ...
+def send_email(mailing):
+    date_time = timezone.now()
+    for objects in mailing:
+        if objects.at_start <= date_time <= objects.at_end and date_time >= objects.next_run:
+            objects.status = 'launched'
+            title = objects.massage.title
+            text = objects.massage.text
+            clients = objects.client.all()
+            clients_email = [client.email for client in clients]
+
+            try:
+                status = send_mail(subject=title,
+                                   message=text,
+                                   from_email=ADDRESS_MAIL_RU,
+                                   recipient_list=clients_email,
+                                   )
+                Attempt.objects.create(status_attempt=status, mailing=objects)
+
+            except Exception as e:
+                Attempt.objects.create(status_attempt=False, mailing=objects, answer_mail=e)
+
+    if mailing.periodicity == 'daily':
+        mailing.next_run = date_time + datetime.timedelta(days=1)
+        mailing.status = 'create'
+    elif mailing.periodicity == 'weekly':
+
+        mailing.next_run = date_time + datetime.timedelta(weeks=1)
+        mailing.status = 'create'
+    elif mailing.periodicity == 'monthly':
+        mailing.next_run = date_time + relativedelta(months=1)
+        mailing.status = 'create'
+    elif mailing.periodicity == 'yearly':
+        mailing.next_run = date_time + relativedelta(years=1)
+        mailing.status = 'create'
+    else:
+        mailing.status = 'completed'
+
+    mailing.save()
+
